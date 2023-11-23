377a232d6d5cc6e5be7d72adb6c36be6e012ca9b
Holger Harms <hello@holhar.de>
2023-11-23 13:30:17 +0100

    initial commit

---
+++ src/main/java/de/holhar/qod/QodApplication.java
@@ -0,0 +1,13 @@
+package de.holhar.qod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QodApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(QodApplication.class, args);
+  }
+
+}
---
+++ src/test/java/de/holhar/qod/QodApplicationTests.java
@@ -0,0 +1,13 @@
+package de.holhar.qod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QodApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
